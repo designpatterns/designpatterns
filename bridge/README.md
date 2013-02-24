c59257ab44b8ca5db9be22f22c78eb405c5a7f63
roman raslin <roman.raslin@gmail.com>
2013-02-24 23:11:29 +0200

    added the bridge pattern

---
+++ bridge/README.md
@@ -0,0 +1,14 @@
+Bridge pattern
+================
+
+### Overview
+
+decouple an abstraction from its implementation so that the two can vary independently
+
+### Reasons to use
+
+- when you want the abstraction seperated from the implementation 
+
+### Source
+
+- http://en.wikipedia.org/wiki/Bridge_pattern
---
+++ bridge/php/bridge.php
@@ -0,0 +1,71 @@
+<?
+//Drawing interface
+interface DrawingAPI {
+    public function drawCircle($dX, $dY, $dRadius);
+}
+ 
+//drawing option 1
+class DrawingAPI1 implements DrawingAPI {
+ 
+    public function drawCircle($dX, $dY, $dRadius) {
+        echo "API1.circle at ".$dX.":".$dY." radius ".$dRadius."<br/>";
+    }
+}
+ 
+//drawing option 2
+class DrawingAPI2 implements DrawingAPI {
+ 
+    public function drawCircle($dX, $dY, $dRadius) {
+        echo "API2.circle at ".$dX.":".$dY." radius ".$dRadius."<br/>";
+    }
+}
+ 
+
+
+
+
+
+/*
+Shape abstract for setting the drawing option only
+*/
+abstract class Shape {
+ 
+    protected $drawingAPI;
+ 
+    protected function __construct(DrawingAPI $_drawingAPI) {
+        $this->drawingAPI = $_drawingAPI;
+    }
+}
+ 
+class CircleShape extends Shape {
+ 
+    private $dX;
+    private $dY;
+    private $dRadius;
+ 
+    public function __construct($dX, $dY,$dRadius, DrawingAPI $oDrawingAPI) {
+        parent::__construct($oDrawingAPI);
+
+        $this->dX = $dX;
+        $this->dY = $dY;
+        $this->dRadius = $dRadius;
+    }
+ 
+    public function draw() {
+        $this->drawingAPI->drawCircle(
+                $this->dX,
+                $this->dY,
+                $this->dRadius
+        );
+    }
+}
+ 
+
+$shapes = array(
+    new CircleShape(1, 3, 7,  new DrawingAPI1()),
+    new CircleShape(5, 7, 11, new DrawingAPI2()),
+);
+
+foreach ($shapes as $shape) {
+    $shape->draw();
+}
