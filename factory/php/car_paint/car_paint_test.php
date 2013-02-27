efb4d9238c159ee9203fec5b19cf7fcca624eba9
Roman Raslin <roman.raslin@gmail.com>
2013-02-27 20:24:41 +0200

    factory pattern structure as agreed

--- factory/php/car_paint/car_paint_test.php
+++ factory/php/car_paint/car_paint_test.php
@@ -1,48 +1,46 @@
 <?php
 
-require_once 'factory\php\examples\factory2.php';
-
-require_once 'PHPUnit\Framework\TestCase.php';
+require_once 'car_paint.php';
 
 /**
  * BMWFactory test case.
  */
 class BMWFactoryTest extends PHPUnit_Framework_TestCase {
-	
+
 	/**
 	 *
 	 * @var BMWFactory
 	 */
 	private $BMWFactory;
-	
+
 	/**
 	 * Prepares the environment before running a test.
 	 */
 	protected function setUp() {
 		parent::setUp ();
-		
+
 		// TODO Auto-generated BMWFactoryTest::setUp()
-		
+
 		$this->BMWFactory = new BMWFactory(/* parameters */);
 	}
-	
+
 	/**
 	 * Cleans up the environment after running a test.
 	 */
 	protected function tearDown() {
 		// TODO Auto-generated BMWFactoryTest::tearDown()
 		$this->BMWFactory = null;
-		
+
 		parent::tearDown ();
 	}
-	
+
 	/**
 	 * Constructs the test case.
 	 */
 	public function __construct() {
 		// TODO Auto-generated constructor
 	}
-	
+
 	/**
 	 * Tests BMWFactory::create()
 	 */
@@ -51,4 +49,3 @@ class BMWFactoryTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('painted to red', BMWFactory::create('red'));
 	}
 }
-
