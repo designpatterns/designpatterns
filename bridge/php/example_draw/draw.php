ddebdb7d9dade08c213eb0cc010a39fe30e89c55
Roman Raslin <roman.raslin@gmail.com>
2013-03-03 09:06:53 +0200

    restructured bridge apttern

--- bridge/php/example_draw/draw.php
+++ bridge/php/example_draw/draw.php
@@ -60,12 +60,3 @@ class CircleShape extends Shape {
     }
 }
  
-
-$shapes = array(
-    new CircleShape(1, 3, 7,  new DrawingAPI1()),
-    new CircleShape(5, 7, 11, new DrawingAPI2()),
-);
-
-foreach ($shapes as $shape) {
-    $shape->draw();
-}
